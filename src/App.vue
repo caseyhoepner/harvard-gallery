89498dd9c3e3bd2408707217e42407eac54f35e9
Casey Hoepner <caseyhoepner@gmail.com>
2019-01-07 20:56:36 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img 
+      id='logo'
+      src="./assets/logo.png" 
+      alt='Harvard Art Museum Logo'
+    >
+    <Gallery/>
+  </div>
+</template>
+
+<script>
+import Gallery from './components/Gallery'
+
+export default {
+  name: 'App',
+  components: {
+    Gallery
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+}
+
+#logo {
+  margin-top: -120px;
+}
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+  <div id="galleryInfo">
+    
+  </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+new Vue({
+  el: '#galleryInfo',
+  data () {
+    return {
+      galleryInfo: null,
+      loading: true,
+      errored: false
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.harvardartmuseums.org/object?apikey=0b7812a0-12e5-11e9-b96e-b96134bf93ea')
+      .then(response => {
+        this.galleryIinfo = response.records
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)
+  }
+})
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
